032e66d396c48068a825d2b949d89c2f4c5206e7
asdf0309-123 <quf090309@gmail.com>
2026-07-07 18:32:20 +0900

    apply secret key

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const io = new Server(server);
 
 const supabase = createClient(
   "https://gktycpztevwrduqkchsxv.supabase.co",
-  "sb_publishable_PKIlIrSBoKor76Jpe8mjVA_9IL3YEtr"
+  "sb_secret_f_-lLofZx0L3ZxgKFRVeJg_GO_fN1Fu"
 );
 
 app.use(express.static("public"));
